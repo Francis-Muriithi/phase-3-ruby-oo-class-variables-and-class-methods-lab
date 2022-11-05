d7a0b66a0135e190b85942855a364f903a38b387
Francis-Muriithi <mufraggie@gmail.com>
2022-11-05 09:23:10 +0300

    resubmission

--- lib/song.rb
+++ lib/song.rb
@@ -1,49 +1,40 @@
-class Song 
+class Song
+    attr_accessor :name, :artist, :genre
 
-@@count = 0
-@@artists = []
-@@genres = []
+    @@count = 0
+    @@genres = []
+    @@artists = []
 
-def self.count 
-    @@count = 3
-end
-def self.artists
-    @@artists.uniq
-end
-def self.genres
-    @@genres.uniq
-end
-
-def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-        if !genre_count[genre]
-        genre_count[genre] = 1
-        else
-        genre_count[genre] += 1
-        end
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
     end
-    genre_count
- end
 
- def self.artist_count
-     artist_count = {}
-     @@artists.each do |artist|
-        if !artist_count[artist]
-            artist_count[artist] = 1
-        else
-            artist_count[artist] += 1
-        end
-     end
-     artist_count
-end
- attr_accessor :name, :artist, :genre
-def initialize (name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-end
-end
+    def self.count
+        @@count
+    end
 
-ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+    # return a unique array of artists && genres of existing songs respectively
+    def self.artists
+        @@artists.uniq
+    end
 
+    def self.genres
+        @@genres.uniq
+    end
+
+    # tally method, counts the occurrences of each element. Returns a hash with the
+    # elements of the collection as keys and the corresponding counts as values.
+    def self.artist_count
+        @@artists.tally
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+end
